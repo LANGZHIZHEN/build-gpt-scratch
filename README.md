dfc2c1e19574dd8f4842f045bc4ec8fa7404ccfd
mod1998 <1511386@mail.nankai.edu.cn>
2026-01-24 21:59:17 +0800

    第七章完成

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # 一周目探索
 ## ch7.指令任务微调
 1.准备数据集，编写Dataset，collate函数，loader <br>
+2.加载预训练权重gpt2-medium
+3.训练后，下载ollama，部署qwen3-4B对test_data进行评估打分（本地进行）
 ## ch6.分类任务微调
 1.准备数据集，保存成csv格式（主要是分好input和target） <br>
 2.编写Dataset类，将text转为词元id，包括每个item的长度保证统一，不足则添加<|endoftext|>；编写Dataloader类<br>
--- utils.py
+++ utils.py
@@ -1,30 +1,14 @@
 import torch
 import numpy as np
-# def calc_loss(input_batch, target_batch, model, device):
-#     input_batch = input_batch.to(device)
-#     target_batch = target_batch.to(device)
-#     logits = model(input_batch)
-#     loss = torch.nn.functional.cross_entropy(
-#         logits.flatten(0, 1),
-#         target_batch.flatten()
-#     )
-#     return loss
-
-# def calc_loss_loader(data_loader, model, device, num_batches=None):
-#     total_loss = 0
-#     if len(data_loader) == 0:
-#         return float('nan')
-#     elif num_batches is None:
-#         num_batches = len(data_loader)
-#     else:
-#         num_batches = min(num_batches, len(data_loader))
-#     for i, (input_batch, target_batch) in enumerate(data_loader):
-#         if i < num_batches:
-#             loss = calc_loss(input_batch, target_batch, model, device)
-#             total_loss += loss.item()
-#         else:
-#             break
-#     return total_loss / num_batches
+def calc_loss(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1),
+        target_batch.flatten()
+    )
+    return loss
 
 def text_to_token_ids(text, tokenizer):
     encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
@@ -123,3 +107,28 @@ def generate(model, idx, max_new_tokens, context_size,
             break
         idx = torch.cat((idx, idx_next), dim=-1)
     return idx
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, 
+                       epochs, eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, step = 0, -1
+    for epoch in range(epochs):
+        model.train()
+        for inputs, targets in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss(inputs, targets, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += inputs.numel()
+            step += 1
+
+            if step % eval_freq == 0:
+                model.eval()
+                with torch.no_grad():
+                    train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                    train_losses.append(train_loss)
+                    val_losses.append(val_loss)
+                    track_tokens_seen.append(tokens_seen)
+                    print(f"Epoch: {epoch}, Step: {step}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+        generate_and_print_sample(model, tokenizer, start_context, device)
+    return train_losses, val_losses, track_tokens_seen, model
